7616f0d174f6a76603516a3f7efa30d36176f354
erikaosguer <1476@holbertonschool.comt>
2020-02-05 22:24:30 +0000

    Size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c *));
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(i *));
+printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(li *));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(lli *));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f *));
+return (0);
+}
