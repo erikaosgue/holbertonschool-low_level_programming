797551000092c7ae9028be5f0deb6382d561279f
erikaosguer <1476@holbertonschool.comt>
2020-02-06 19:10:44 +0000

    assing a random number --version 3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,12 +16,12 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-printf("is positive\n");
+printf("is positive\n", n);
 
 else if (n == 0)
-printf("is zero\n");
+printf("is zero\n", n);
 
 else
-printf("is negative\n");
+printf("is negative\n", n);
 return (0);
 }
