7ff706c7127033adafde80bbe89764940486d0dd
erikaosguer <1476@holbertonschool.comt>
2020-02-06 20:52:14 +0000

    find the last digit of a number --version 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,13 +15,20 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
-	
 	if (lastdigit > 5)
-        printf("last digit of %d is %d and is greater than 5\n", n, lastdigit);
-	if else (lastdigit == 0)
-	printf("last digit of %d\n is %d\n and is 0\n", n, lastdigit);
+	{
+		printf("last digit of %d is %d and is greater than 5\n",
+				n, lastdigit);
+	}
+	else if (lastdigit == 0)
+	{
+		printf("last digit of %d is %d and is 0\n",
+				n, lastdigit);
+	}
 	else
-	printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+	{
+		printf("last digit of %d is %d and is less than 6 and not 0\n",
+				n, lastdigit);
+	}
 	return (0);
 }
-
