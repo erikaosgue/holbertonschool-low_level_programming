39aa7e6e7947a2842f1beaf8a9ac1ae0ee06704e
erikaosguer <1476@holbertonschool.comt>
2020-02-07 00:17:17 +0000

    print numbers in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char a;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(i + '0');
+	}
+	
+	for (a = 'a'; a <= 'f'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
