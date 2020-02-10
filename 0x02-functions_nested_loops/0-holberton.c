1ddf47ae1acf009a3c2f0d5d7689dd076cbc41e0
erikaosguer <1476@holbertonschool.comt>
2020-02-10 23:04:17 +0000

    Creates a function putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - burger
+ * 
+ * Result: Entry point acces
+ */
+int main(void)
+{
+ 	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
