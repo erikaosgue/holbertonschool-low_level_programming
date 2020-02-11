6b441caecbfea98259dbb7a29b51321aa28b588b
erikaosguer <1476@holbertonschool.com>
2020-02-11 04:28:20 +0000

    Computes the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs: check if the number is greater than 0
+ * @n: the number to be checked
+ * Return: Positive for all negative and postive ones
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n = n * -1;
+		return (n);
+	}
+
+}
