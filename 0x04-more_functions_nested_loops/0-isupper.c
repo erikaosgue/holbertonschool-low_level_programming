30fcde0413a120ad9d9043d6f3e060d7f57e42f3
erikaosguer <1476@holbertonschool.comt>
2020-02-13 17:56:57 +0000

    Checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _isupper - checks uppercase character
+ * @c: is a character
+ * Return: 1 if it is a uppercase letter, 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c == 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
