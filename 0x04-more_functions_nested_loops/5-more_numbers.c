74feee56d34359aa0fae554985f2f65771d19484
erikaosguer <1476@holbertonschool.comt>
2020-02-13 22:56:04 +0000

    Print the numbers until 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * more_numbers - print the numbers from 0 to 9 10 times 
+ * more_numbers - print the numbers from 0 to 9 10 times
  *
  * Return: 0 always
  */
@@ -10,11 +10,11 @@ void more_numbers(void)
 	int j = 0;
 	int i = 0;
 
-	for (i = 0; i <= 9; i++);
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
-		{	
-			if (j >=10)
+		{
+			if (j >= 10)
 			{
 				_putchar('1');
 			}
