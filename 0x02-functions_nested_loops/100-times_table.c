e1ff207ed444eaad53816e92283780080c5e7587
erikaosguer <1476@holbertonschool.com>
2020-02-14 03:19:17 +0000

    Prints n table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,13 +19,21 @@ void print_times_table(int n)
 				m = i * j;
 				if (j == 0)
 					_putchar(m + '0');
-				else if (m > 9)
-				{		
+				else if (m > 9 && m < 100)
+				{
+					_putchar(' ');
 					_putchar((m / 10) + '0');
+					_putchar((m % 10) + '0');
+				}
+				else if (m > 99)
+				{
+					_putchar((m / 100 % 10) + '0');
+					_putchar((m / 10 % 10) + '0');
 					_putchar((m % 10) + '0');
 				}
 				else
 				{
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(m + '0');
 				}
@@ -33,6 +41,7 @@ void print_times_table(int n)
 					break;
 				_putchar(',');
 				_putchar(' ');
+				
 			}		
 			_putchar('\n');
 		}
