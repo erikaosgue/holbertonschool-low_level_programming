6fd026c1fe959005c6333edbcb9424c0415923ed
erikaosguer <1476@holbertonschool.comt>
2020-02-14 00:21:42 +0000

    Print the numbers to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print the number to 100
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		a = i % 3;
+		b = i % 5;
+		if (a == b)
+		{
+			printf("%d ", i);
+		}
+		else if (a == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (b == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz");
+	return (0);
+}
