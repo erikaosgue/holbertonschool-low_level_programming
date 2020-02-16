890b3825a14e313d9b0b9a920fa0560d2f7339e5
erikaosguer <1476@holbertonschool.com>
2020-02-16 03:04:39 +0000

    Sum of multiples 5 and 3 of 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int i;
+	int a = 1024;
+	int c;
+	int d;
+	int sum = 0;
+
+	for (i = 0; i < a; i++)
+	{
+		c = i * 3;
+		d = i * 5;
+		if (d < a)
+		{
+			sum = sum + d;
+		}
+		if (c < a && c % 10 !=5 && c % 10 != 0)
+		{
+			sum = sum + c;
+			
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
