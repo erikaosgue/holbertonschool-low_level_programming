87189d316504498cb39e7be566a1c11f0f58c5f2
erikaosguer <1476@holbertonschool.comt>
2020-02-18 20:53:10 +0000

    Print Numbers follow by a comma and space

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - Print Numbers with comma and space
+ * @a: Is the address of the string
+ * @n: is the size of the array
+ * Return: 0 Always
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+			break;
+		}
+	}
+}
