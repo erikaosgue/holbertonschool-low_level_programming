aa26e012c43427e20b7ea8baba287c87ac427908
erikaosguer <1476@holbertonschool.comt>
2020-02-19 00:16:33 +0000

    Project number 5

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+int _atoi(char *s)
+{
+	int j;
+	int i;
+	int k;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] >= 48; && s[i] <= 57);
+		{
+
+		}
+	}
+
+}
