9e11d85557edd28fc2d6c2fd0a8f924f314919bb
erikaosguer <1476@holbertonschool.com>
2020-02-21 03:54:47 +0000

    Encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,21 +1,26 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * leet - Encodes a string into 1337
+ * @s: Address of the string
+ * Return: The addres of the string with modifications
  */
 char *leet(char *s)
 {
 	int i;
 	int j;
-	char st[6] = {'4', '3', '0', '7', '1'} 
+	char s2[10] = {'4', '4', '3', '3', '1', '1', '0', '0', '7', '7'};
+	char s3[10] = {'a', 'A', 'e', 'E', 'l', 'L', 'o', 'O', 't', 'T'};
 
-	for (i = 0; s[i] != 0; i++) 
+	for (i = 0; s[i] != 0; i++)
 	{
-		for (j = 0; st[j] != 0; j++)
+		for (j = 0; s2[j] != 0; j++)
 		{
-			if (st[]
+			if (s[i] == s3[j])
+			{
+				s[i] = s2[j];
+			}
 		}
 	}
+	return (s);
 }
 
