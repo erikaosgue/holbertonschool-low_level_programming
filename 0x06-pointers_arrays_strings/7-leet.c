05640b5117e9be16434d55d9449d7812a6956503
erikaosguer <1476@holbertonschool.comt>
2020-02-21 00:50:46 +0000

    task number 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char st[6] = {'4', '3', '0', '7', '1'} 
+
+	for (i = 0; s[i] != 0; i++) 
+	{
+		for (j = 0; st[j] != 0; j++)
+		{
+			if (st[]
+		}
+	}
+}
+
