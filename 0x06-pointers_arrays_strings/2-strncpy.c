f2b182dd7ef86872570f2e602ba622dcf4491ff9
erikaosguer <1476@holbertonschool.com>
2020-02-22 04:24:34 +0000

    Copies a string into another one

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,18 +3,19 @@
  * _strncpy - Copies a string into another one
  * @dest: String destiny to paste the string source
  * @src: String source to copy the string
+ * @n: Number of bytes the string src should copy
  * Return: The address of the string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while(i < n && src[i] != 0)
+	while (i < n && src[i] != 0)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while(i < n)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
