829b7ff7aea175bf1a44f8f443fa458aa935aaeb
erikaosguer <1476@holbertonschool.comt>
2020-02-24 18:20:01 +0000

    Locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,22 @@
 #include "holberton.h"
 /**
- * _srtchr - Locates a character in a string
- * @s: Address of the array 
+ * _strchr - Locates a character in a string
+ * @s: Address of the array
  * @c: Character
  * Return: The string modified
  */
 char *_strchr(char *s, char c)
 {
 	int i;
-	int j;
 
 	for (i = 0; s[i] != 0; i++)
 	{
 		if (s[i] == c)
 		{
-			for (j = 0; s[i] != 0; j++)
-			{
-				s[j] = s[i];
-				i++;
-			}
+			s = s + i;
+			break;
 		}
+
 	}
 	return (s);
 }
