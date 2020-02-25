596a7b9e9385b6360a4f7781949e02798b4a5d9b
erikaosguer <1476@holbertonschool.com>
2020-02-25 04:21:54 +0000

    Fix the requirement for the check

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,9 +10,6 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	if (needle == 0)
-		return (haystack);
-
 	for (i = 0; haystack[i] != 0; i++)
 	{
 		for (j = 0; needle[j] != 0; j++)
