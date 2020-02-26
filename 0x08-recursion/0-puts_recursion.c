1508b78f68f97950396a9b4284c3da570be904b4
erikaosguer <1476@holbertonschool.comt>
2020-02-26 18:45:26 +0000

    prints a string in reverse.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: Addres of the imput string
+ * Return: 0 Always
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+
+
+
+}
