c4ff0672889aed50b89c2c65e5dee87fffba5f7a
erikaosguer <1476@holbertonschool.comt>
2020-02-27 00:49:40 +0000

    Project 8

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - returns the value of x raised to the power y
+ * @x: Base of the exponential number
+ * @y: It is the power of the number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (x * _pow_recursion(x, y - 1));
+
+
+}
