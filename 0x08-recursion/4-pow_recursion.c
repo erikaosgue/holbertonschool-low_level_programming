41569c50ed970540eb396c04bee848301fca8ec6
erikaosguer <1476@holbertonschool.com>
2020-02-27 04:16:27 +0000

    The square root function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,13 +3,15 @@
  * _pow_recursion - returns the value of x raised to the power y
  * @x: Base of the exponential number
  * @y: It is the power of the number
+ * Return: Integer, result of the x to the power y
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	else if (y == 0)
-		return (x * _pow_recursion(x, y - 1));
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 
 
 }
