b720a7e06eafa47a694f38c09053b0a5fa278fa3
erikaosguer <1476@holbertonschool.com>
2020-02-28 03:25:16 +0000

    Return 1 if a number is prime, 0 other wise

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,29 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * is_prime_number - Find the prime number returning 1
+ * @n: Number given
+ * Return: 1 if the number is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
+	if (n <= 1)
+		return (0);
 	return (prime_number(n, 2));
 }
+/**
+ * prime_number - Find the prime number returning 1
+ * @n: Number given
+ * @i: integer that works as an increment
+ * Return: 1 if the number is prime, 0 otherwise
+ */
 
-int prime_number(int num, int i)
+int prime_number(int n, int i)
 {
-	if (num < i)
-		return (0);
-	else if (num == i)
+	if (i == n)
 		return (1);
-	else if (num / i && num != i)
+	else if (n % i == 0)
 		return (0);
-	else if (num / i != 0 && num > i)
-		return (prime_number(num, i + 1));
+	else
+		return (prime_number(n, i + 1));
+	return (1);
 }
