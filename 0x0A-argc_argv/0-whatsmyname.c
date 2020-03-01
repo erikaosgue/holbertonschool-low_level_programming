8bade717910eca36382d4cac0a6f7dbaa15b78d6
erikaosguer <1476@holbertonschool.com>
2020-03-01 05:27:59 +0000

    first version

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Print the name's file
+ * @argc: Number os arguments in command line
+ * @argv: Argument vector
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	
+	printf("%s\n", argv[i]);
+	(void)argc;
+
+	return (0);
+}
