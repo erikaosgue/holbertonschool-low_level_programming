f14091c588ed4b23a78a1304ec5653d0f66e1b7c
erikaosguer <1476@holbertonschool.comt>
2020-03-03 22:00:40 +0000

    second version

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,12 +22,8 @@ int **alloc_grid(int width, int height)
 		ptr[i] = malloc(sizeof(int) * width);
 		if (ptr == 0)
 		{
-			free();
 			return (0);
 		}
-	}
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 		{
 			ptr[i][j] = 0;
