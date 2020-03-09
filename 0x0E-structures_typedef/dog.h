ef11c03c763f114a593e51e34d41140fa0407abc
erikaosguer <1476@holbertonschool.comt>
2020-03-09 22:20:38 +0000

    First version Header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: address of the owner string
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
