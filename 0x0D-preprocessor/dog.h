900924e7995b792330001b77af610d0de3bd7250
erikaosguer <1476@holbertonschool.comt>
2020-03-09 22:03:33 +0000

    First version

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+/**
+ * struct dog - struc dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif 
