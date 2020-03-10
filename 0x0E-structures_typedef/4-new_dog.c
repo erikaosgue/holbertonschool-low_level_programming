d9edeb1887d458c62b80a9702a656d04afee6960
erikaosguer <1476@holbertonschool.comt>
2020-03-10 00:35:59 +0000

    First version

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *s;
+	unsigned int size = 0;
+
+	if (str == 0)
+		return (0);
+	for (i = 0; str[i] != 0; i++)
+	{
+		size += 1;
+	}
+	s = malloc(sizeof(char) * (size + 1));
+	if (s == 0)
+		return (0);
+	for (i = 0; i < size; i++)
+	{
+		s[i] = str[i];
+	}
+	s[i] = 0;
+	return (s);
+}
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	dog = malloc(sizeof(dog_t));
+	if (dog == 0)
+		return (0);
+	dog->name = _strdup(name);
+	if (dog->name == 0)
+	{
+		free(dog);
+		return (0);
+	}
+	dog->age = age;
+	dog->owner = _strdup(owner);
+	if (dog->owner == 0)
+	{
+		free(dog);
+		return (0);
+	}
+	return (dog);
+}
