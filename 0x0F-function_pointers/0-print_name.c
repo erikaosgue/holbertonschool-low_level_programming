593dcffa358b8e8bbd41c45e7a73d13e844b86b2
erikaosguer <1476@holbertonschool.comt>
2020-03-11 18:18:04 +0000

    Add verification of NULL arguments

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,13 @@
  * print_name - takes char pointer parameter and a pointer function
  * @name: address of the string name
  * @f: pointer function f
- * Return: 0 
+ * Return: 0
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == 0)
+		return;
+	if (f == 0)
+		return;
 	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == 0)
+		return;
+	if (action == 0)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
