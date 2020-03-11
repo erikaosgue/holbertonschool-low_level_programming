d5415b50ee46b7af9b1b37115c07b1ce9e38ed5b
erikaosguer <1476@holbertonschool.comt>
2020-03-11 19:40:45 +0000

    Fix the conditional inside the for

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "function_pointers.h"
 /**
  * int_index - function that searches for an integer
@@ -15,10 +16,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (0);
 	if (cmp == 0)
 		return (0);
+	if (size <= 0)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return (i);
+		return (i);
 	}
-	return (0);
+	return (-1);
 }
