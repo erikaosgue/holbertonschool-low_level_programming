dfff4194b1bacafa389833bc07136375d904bdaf
erikaosguer <1476@holbertonschool.comt>
2020-03-12 00:16:23 +0000

    Fix initialization of numbers num1 and num2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,17 +18,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1] != 0)
+	{
+		printf("Error\n");
+		return (0);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
-	}
-	if (argv[2][1] != 0)
-	{
-		printf("Error\n");
-		return (0);
 	}
 	result = get_op_func(argv[2])(num1, num2);
 	printf("%d\n", result);
