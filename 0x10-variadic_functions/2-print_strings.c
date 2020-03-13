1afb2e9bbcc189e79ef4a313508e68de8bca63ec
erikaosguer <1476@holbertonschool.comt>
2020-03-13 21:43:32 +0000

    Add the condition If string is NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "variadic_functions.h"
 /**
- * print_numbers - Print each number fallow by the string given
+ * print_strings - Print each string follow by a separator
  * @separator: addres of the string given
  * @n: is the number of arguments passed
  * Return: Nothing
@@ -12,13 +12,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i = 0;
+	char *ptr;
 
 	va_start(list, n);
 
-	for (i = 0; i < n - 1; i++)
+	if (separator != 0)
 	{
-		printf("%s%s", va_arg(list, char *), separator);
+		for (i = 0; i < n; i++)
+		{
+			ptr = va_arg(list, char *);
+			if (ptr != 0)
+				printf("%s", ptr);
+			else
+				printf("nil");
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
 	}
-	printf("%s\n", va_arg(list, char *));
 	va_end(list);
 }
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - Print each number fallow by the string given
+ * @separator: addres of the string given
+ * @n: is the number of arguments passed
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i = 0;
+	
+}
+
