68cf9d2dfa216047fed76bdd6497ebef4a438638
erikaosguer <1476@holbertonschool.com>
2020-03-14 17:55:11 +0000

    Fix the condition if separator NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i = 0;
 
-	if (separator == 0)
-		return;
-	va_start(list, n);
-
-	for (i = 0; i < n - 1; i++)
+	if (separator != 0)
 	{
-		printf("%d%s", va_arg(list, unsigned int), separator);
+		va_start(list, n);
+
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(list, unsigned int));
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
 	}
-	printf("%d\n", va_arg(list, unsigned int));
 	va_end(list);
 }
