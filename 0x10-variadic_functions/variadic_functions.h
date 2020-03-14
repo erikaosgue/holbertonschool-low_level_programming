19086f1bf179647a0f27e5514475f9cc1e8914b0
erikaosguer <1476@holbertonschool.com>
2020-03-14 03:43:02 +0000

    Second version

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,4 @@
-#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
 /**
@@ -9,13 +9,15 @@
 typedef struct format
 {
 	char *format;
-	int (*funcion)(va_list list);
+	void (*funcion)(va_list list));
 } srtuct_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-
-
+void func_char(va_list);
+void func_int(va_list);
+void func_float(va_list);
+void func_pointer(va_list);
 #endif
