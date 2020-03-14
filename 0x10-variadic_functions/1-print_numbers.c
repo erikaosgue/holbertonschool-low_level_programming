d0ecddd56f02b88eea58b4c7f6ecccdee9232db3
erikaosguer <1476@holbertonschool.com>
2020-03-14 18:22:07 +0000

    Fix betty requirments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,13 +12,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
-	
+
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, unsigned int));
 		if (i < n - 1 && separator != 0)
-				printf("%s", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(list);
