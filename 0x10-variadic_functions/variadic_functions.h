5f9633d3e7a9c1d7818b806cf348676d315113c5
erikaosguer <1476@holbertonschool.com>
2020-03-14 17:17:14 +0000

    Correct the Header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,23 +1,13 @@
-#define VARIADIC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
-/**
- * struct op - Struct op
- * @op: The operator
- * @f: The function associated
- */
-typedef struct format
-{
-	char *format;
-	void (*funcion)(va_list list));
-} srtuct_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-void func_char(va_list);
-void func_int(va_list);
-void func_float(va_list);
-void func_pointer(va_list);
+void func_char(char);
+void func_int(int);
+void func_float(float);
+void func_pointer(char *);
 #endif
