4baf3cad28a20cadc95efe8f5ca7e107e8101ffb
erikaosguer <1476@holbertonschool.com>
2020-03-16 05:39:45 +0000

    Fix betty requirements

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(list, n);
 
-	if (n == 0)
-		return (0);
-
-	for (i = 0; i < n; i++)
+	if (n != 0)
 	{
-		sum += va_arg(list, int);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(list, int);
+		}
+		return (sum);
 	}
 	va_end(list);
-	return (sum);
+	return (0);
 }
