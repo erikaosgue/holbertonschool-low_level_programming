29d16d238df638875d92ff91481011d0d02ef814
erikaosgue <1476@holbertonschool.com>
2020-03-25 03:55:59 +0000

    Fix betty requirements

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - deletes the node at index index
+ * of a listint_t linked list
  * @head: Pointer to the address of the pointer head
  * @index:  is the index of the node to delete
  * Return: the address of the new element, or NULL if it failed
