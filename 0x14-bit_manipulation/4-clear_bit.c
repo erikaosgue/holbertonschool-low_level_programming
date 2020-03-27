d09ede75a29c04136f3be2d58502eadaca95d9d5
erikaosgue <1476@holbertonschool.com>
2020-03-27 01:54:50 +0000

    First version that clears a bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	*n = *n & (~i << index);
+	*n = *n & ~(i << index);
 	return (1);
 
 }
