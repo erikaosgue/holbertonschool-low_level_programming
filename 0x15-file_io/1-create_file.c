d2fa79497951cef27ab3b33be1c7a4198abb9558
erikaosgue <1476@holbertonschool.com>
2020-04-01 01:27:00 +0000

    Remove the function strlen

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,20 +4,6 @@
 #include <unistd.h>
 #include "holberton.h"
 /**
- * _strlen - counts the len of the string
- * @str: pointer to the string
- * Return: the len of the string
- */
-int _strlen(char *str)
-{
-	int len;
-
-	for (len = 0; str[len] != 0; len++)
-	{
-	}
-	return (len);
-}
-/**
  * create_file - creates a file
  * @filename: is the name of the file to create
  * @text_content:  is a NULL terminated string to write to the file
@@ -25,7 +11,7 @@ int _strlen(char *str)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0, bytes = 0, n_letters = 0;
+	int fd = 0, bytes = 0, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -38,16 +24,16 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		printf("fails, file couldn't be open\n");
 		return (-1);
 	}
 
-	n_letters = _strlen(text_content);
+	for (len = 0; text_content[len] != 0; len++)
+	{
+	}
 
-	bytes = write(fd, text_content, n_letters);
+	bytes = write(fd, text_content, len);
 	if (bytes == -1)
 	{
-		printf("fail, file couldn't be written\n");
 		return (-1);
 	}
 	close(fd);
