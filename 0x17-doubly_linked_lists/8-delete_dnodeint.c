b9501449876506c2b4959c5fd4ef8a51551fd566
erikaosgue <1476@holbertonschool.com>
2020-05-02 19:46:49 +0000

    Add condition when index doesnt exist

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+ * dlistint_len - Count the number of elements in a linked dlistint_t list
+ * @h:Points to the node which is the head of the list
+ * Return: the number of elements in a linked dlistint_t list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count_node = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		count_node += 1;
+		h = h->next;
+	}
+	return (count_node);
+}
+/**
  * delete_dnodeint_at_index - A function that deletes the node
  * at index index of a dlistint_t linked list.
  * @head: Points to the head pointer that points to the head node of the list
@@ -9,36 +27,41 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = NULL, *previous = NULL, *temp = NULL;
-	unsigned int count = 0;
+	unsigned int count = 0, len_list = 0;
 
 	current = previous = *head;
 	if (head == NULL || *head ==  NULL)
 		return (-1);
-	while (current)
+	len_list = dlistint_len(*head);
+	if (index < len_list)
 	{
-		if (index == 0)
+		while (current)
 		{
-			if (current->next == NULL)
+			if (index == 0)
 			{
-				free(current), *head = NULL;
+				if (current->next == NULL)
+				{
+					free(current), *head = NULL;
+					return (1);
+				}
+				temp = current, current = current->next, free(temp);
+				current->prev = NULL, *head = current;
 				return (1);
 			}
-			temp = current, current = current->next, free(temp);
-			current->prev = NULL, *head = current;
-			return (1);
-		}
-		count += 1, previous = current, current = current->next;
-		if (current->next == NULL && index == count)
-		{
-			previous->next = NULL, free(current);
-			return (1);
-		}
-		if (index == count)
-		{
-			temp = current, current = current->next, free(temp);
-			previous->next = current, current->prev = previous;
-			return (1);
+			count += 1, previous = current, current = current->next;
+			if (current->next == NULL && index == count)
+			{
+				previous->next = NULL, free(current);
+				return (1);
+			}
+			if (index == count)
+			{
+				temp = current, current = current->next, free(temp);
+				previous->next = current, current->prev = previous;
+				return (1);
+			}
 		}
 	}
+	printf("Indexdoesn't exist\n");
 	return (-1);
 }
