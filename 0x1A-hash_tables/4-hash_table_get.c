53a1af93c60eba069e9a132731e235daccc2e544
erikaosgue <1476@holbertonschool.com>
2020-05-29 18:26:18 +0000

    Add condition if key is empty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *head = NULL;
 
+
+	if (strcmp(key, "") == 0 || key == '\0')
+		return (0);
+
 	index = key_index((unsigned char *)key, ht->size);
 	head = ht->array[index];
 	while (head)
