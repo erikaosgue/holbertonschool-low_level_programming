1595eca81b1f6faddb600dd69ed9d04b40a21ba0
erikaosgue <1476@holbertonschool.com>
2020-07-03 20:40:58 +0000

    Task 5: Hotfix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,6 +14,8 @@ def island_perimeter(grid):
         [int]: [perimeter of the island in grid]
     """
     perimeter = 0
+    if grid is None:
+        return perimeter
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
