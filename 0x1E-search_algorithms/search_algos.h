8b649a553860af19f0df261863780e7512f4bbe0
erikaosgue <1476@holbertonschool.com>
2020-08-27 04:06:49 +0000

    Add header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,6 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -12,5 +10,7 @@ int binary_search(int *array, size_t size, int value);
 int binary_recursive(int *array, size_t left, size_t right, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
