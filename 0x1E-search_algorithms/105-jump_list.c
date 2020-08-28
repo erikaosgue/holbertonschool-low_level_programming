354fd3d40170f71314dd63ffb09ff65cc9419343
erikaosgue <1476@holbertonschool.com>
2020-08-28 01:42:24 +0000

    Add task 12: Jump search Update

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,47 +1,43 @@
 #include "search_algos.h"
 
 /**
- * jump_list -searches for a value in a sorted list of integers using the Jump
+ * jump_list -searches for a value in a sorted list of integers using the jump
  * search algorithm.
  * @list: is a pointer to the head of the list to search in
  * @size: is the number of nodes in list
  * @value: is the value to search for
- * Return: NULL If value is not present in head or if head is NULL,
- * otherwise return a pointer to the first node where value is located
+ * Return: NULL, If value is not present in head or if head is NULL,
+ * otherwise return a pointer to the first head where value is located
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	listint_t *head = NULL, *before = NULL;
-	size_t step = 0, i = 0;
+	size_t i = 0, step = 0;
+	listint_t *before, *head;
 
-	if (!list || size == 0)
+	if (list == NULL || size == 0)
 		return (NULL);
 
-	i = step = sqrt(size);
+	step = sqrt(size);
 	before = head = list;
-
-	while (head->n < value && head->index < size - 1)
+	for (; head->index < size - 1 && head->n < value;)
 	{
 		before = head;
-		while (head->index < step)
+		for (i += step; head->index < i; head = head->next)
 		{
-			head = head->next;
 			if (head->index == size - 1)
 				break;
 		}
-		step += i;
-		printf("Value checked at index[%ld] = [%d]\n", head->index, head->n);
+		printf("Value checked at index [%ld] = [%d]\n", head->index, head->n);
 	}
+
 	printf("Value found between indexes [%ld] and [%ld]\n",
 	before->index, head->index);
 
-	while (before->index < head->index && before->n < value)
-	{
-		printf("Value checked at index[%ld] = [%d]\n", before->index, before->n);
-		before = before->next;
-	}
-	printf("Value checked at index[%ld] = [%d]\n", before->index, before->n);
-	if (before->n == value)
-		return (before);
-	return (NULL);
+	for (; before->index < head->index && before->n < value;
+	before = before->next)
+		printf("Value checked at index [%ld] = [%d]\n",
+		before->index, before->n);
+	printf("Value checked at index [%ld] = [%d]\n", before->index, before->n);
+
+	return (before->n == value ? before : NULL);
 }
